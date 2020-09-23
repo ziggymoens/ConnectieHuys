db09a22eb6ffb771d38326d77c352935b497294e
KilianHoefman <kilian.hoefman@student.hogent.be>
2020-09-23 17:47:49 +0200

    confirmationpage for php emails

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_errors',1);  
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $mailFrom = $_POST['mail'];
@@ -10,37 +10,17 @@ if(isset($_POST['submit'])){
     $message = $_POST['message'];
 
     $mailTo = "info@connectiehuys.be";
-    $headers = "Je hebt een email ontvangen van ".$fname." ".$lname." vanuit het contactformulier!\n\n";
-    $txt = "Email: ".$mailFrom."\n
-    \nPersoon heeft volgend bericht meegegeven: \n\n".$message;
+    $headers = "Je hebt een email ontvangen van " . $fname . " " . $lname . " vanuit het contactformulier!\n\n";
+    $txt = "Email: " . $mailFrom . "\n
+    \nPersoon heeft volgend bericht meegegeven: \n\n" . $message;
 
     $sent = mail($mailTo, $subject, $txt, $headers);
     // header("Location: ../contact.html?mailsent");
 }
 if ($sent) {
+    header("Location: ../confirm.html?mailsent");
 
-    ?><html>
-    <head>
-    <title>Thank You</title>
-    </head>
-    <body>
-    <h1>Thank You</h1>
-    <p>Thank you for your feedback.</p>
-    </body>
-    </html>
-    
-    <?php
-    
-    } else {
-    ?><html>
-    <head>
-    <title>Something went wrong</title>
-    </head>
-    <body>
-    <h1>Something went wrong</h1>
-    <p>We could not send your feedback. Please try again.</p>
-    </body>
-    </html>
-    <?php
-    }
-    ?>
+} else {
+    header("Location: ../fail.html?mailnotsent");
+}
+?>
